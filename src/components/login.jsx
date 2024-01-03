43a831c0d3d87d60ff70af7997acc64508da740e
Nithish <88189360+nithish73588@users.noreply.github.com>
2024-01-03 18:30:53 +0530

    yeah

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,18 +1,20 @@
 import React, { useState } from 'react';
+import Image from '../assets/bg1.jpg';
 
 const Login = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+//   const [username, setUsername] = useState('');
+//   const [password, setPassword] = useState('');
 
-  const handleLogin = () => {
+//   const handleLogin = () => {
     
-    console.log('Username:', username);
-    console.log('Password:', password);
-  };
+//     console.log('Username:', username);
+//     console.log('Password:', password);
+//   };
 
   return (
-   <>
+   <div style={{background:`url(${Image})`}}>
    <div className='grid place-items-center h-screen bg-white-500'>
+
         <form className="flex flex-col gap-5 bg-[#006DA1] py-8 px-10 p-1 rounded-md">
             <h1 className='pb-10 text-center text-white font-bold text-2xl'>Login Page</h1>
             <input type='text' placeholder='Enter Your Email' className='p-1 rounded-md' />
@@ -21,7 +23,7 @@ const Login = () => {
             <input type='submit' value='LOGIN' className='bg-red-300 p-1 rounded-md'/>
         </form>
    </div>
-   </>
+   </div>
   );
 };
 
