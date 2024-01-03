83079a59046f92b905fea11da53a7a252551b472
Vijay Anand S <83589426+vijayanand-006@users.noreply.github.com>
2024-01-03 17:57:43 +0530

    Register

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -3,8 +3,18 @@ import React from "react";
 const Register = () => {
     return (
         <>
-            <div>
-
+            <div className="grid place-items-center h-screen">
+                    <form className="flex flex-col text-center px-10 bg-green-400 rounded">
+                        <h1 className="text-7xl">Register</h1>
+                        <input type="text" placeholder="First Name"/>
+                        <input type="text" placeholder="Last Name"/>
+                        <input type="Date" placeholder="BOD"/>
+                        <input type="mail" placeholder="Email"/>
+                        <input type="text" placeholder="Phone NUmber"/>
+                        <input type="text" placeholder="Password"/>
+                        <input type="text" placeholder="Password"/>
+                        <input type="submit" value="Submit"/>
+                    </form>
             </div>
         </>
     )
