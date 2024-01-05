48223ac9351dbc8f4a3533422b64efa420d391c4
Vijay Anand S <83589426+vijayanand-006@users.noreply.github.com>
2024-01-05 11:13:28 +0530

    Header and nav

--- src/components/index/header.jsx
+++ src/components/index/header.jsx
@@ -1,23 +1,70 @@
 import React from "react";
+import { GoHeart } from "react-icons/go";
+import { IoCartOutline } from "react-icons/io5";
 
 const Header = () => {
+  function Car(props) {
+    return (
+      <li>
+        <a href={`#${props.brand}`}>{props.brand}</a>
+      </li>
+    );
+  }
+
+  const cars = [
+    { id: 1, brand: "Home" },
+    { id: 2, brand: "Contact" },
+    { id: 3, brand: "About" },
+    { id: 4, brand: "Sign Up" },
+  ];
+
   return (
-    <div className="bg-black">
-      <div className="max-w-[1240px] mx-auto py-[12px] ">
+    <div>
+      <div className="bg-black">
+        <div className="max-w-[1240px] mx-auto py-[12px] ">
+          {/* Offer show Header */}
+
+          <div className="max-w-[880px] ml-auto">
+            <div className="text-white flex justify-between">
+              <div className="flex">
+                <p>
+                  Summer Sale For All Swim Suits And Free Express Delivery - OFF
+                  50%!
+                </p>
+                <a href="/" className="pl-2">
+                  ShopNow
+                </a>
+              </div>
+              <div className="">
+                <select className="bg-black tetx-white">
+                  <option selected>English</option>
+                </select>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="text-black flex justify-between max-w-[1240px] mt-[40px] mb-[16px] mx-auto">
         <div className="">
-          <div className="text-white flex justify-between">
-            <div className="flex">
-              <p>
-                Summer Sale For All Swim Suits And Free Express Delivery - OFF
-                50%!
-              </p>
-              <a href="/" className="pl-5">ShopNow</a>
-            </div>
-            <div className="">
-              <select className="bg-black tetx-white">
-                <option selected>English</option>
-              </select>
-            </div>
+          {" "}
+          {/* Topic Header */}
+          <h2>Exclusive</h2>
+        </div>
+        <div>
+          {" "}
+          {/* Navigation */}
+          <ul className="flex gap-12">
+            {cars.map((car) => (
+              <Car key={car.id} brand={car.brand} />
+            ))}
+          </ul>
+        </div>
+        <div>
+          {/* Search , Cart , Fav */}
+          <div className="flex items-center gap-x-4 text-black">
+            <input type="Search" placeholder="what are you looking for?" />
+            <GoHeart />
+            <IoCartOutline />
           </div>
         </div>
       </div>
